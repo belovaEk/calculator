21634750d4df73e2c42f76d711596fadb60736b7
Kate <belova.ek0@gmail.com>
2026-02-25 13:05:46 +0500

    Настройка api для общения с сервером

---
+++ calculator-app/src/shared/api/http.ts
@@ -0,0 +1,28 @@
+import axios from "axios";
+
+const api: string = import.meta.env.VITE_BACK_HOST;
+
+export const http = axios.create({
+    baseURL: api,
+    withCredentials: true,
+});
+
+export async function fetchPost<T, R>(url: string, data: T): Promise<R> {
+    try {
+        const response = await http.post<R>(url, data);
+        return response.data;
+    } catch (error) {
+        console.log('POST request failed:', error)
+        throw error;
+    }
+};
+
+export async function fetchGet <T>(url: string): Promise<T> {
+  try {
+    const response = await http.get<T>(url);
+    return response.data;
+  } catch (error) {
+    console.error('GET request failed:', error);
+    throw error;
+  }
+};
