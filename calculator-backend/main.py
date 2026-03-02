3eb09e3792ed23eaba082bc71b984f79cefa6979
Kate <belova.ek0@gmail.com>
2026-03-03 00:20:46 +0500

    Изменение портов и настройка docker

--- calculator-backend/main.py
+++ calculator-backend/main.py
@@ -42,4 +42,4 @@ app = start_application()
 if __name__ == "__main__":
     # Используем строку "main:app", чтобы uvicorn
     # мог корректно перезапускать процесс с reload.
-    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
