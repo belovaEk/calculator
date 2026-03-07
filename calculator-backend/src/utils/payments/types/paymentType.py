bf36f7a23b56d417ae98753d37b9483c0e6a5084
Vladislav <winr2005@gmail.com>
2026-03-07 19:44:18 +0500

    Изменение типа PeriodAmount

--- calculator-backend/src/utils/payments/types/paymentType.py
+++ calculator-backend/src/utils/payments/types/paymentType.py
@@ -1,11 +1,12 @@
 from ast import List
 from typing import  Dict, TypeAlias
-from src.schemas.json_query_schema import PeriodType
-from src.schemas.json_query_schema import PeriodType
+from src.schemas.json_query_schema import PeriodType, PensionCategoryRaw
+
 AmountByYear: TypeAlias = Dict[int, float]
 
 class PeriodAmount(PeriodType):
     amount: float
+    type: PensionCategoryRaw
 
 
 PaymentsByYear: TypeAlias = Dict[int, List[PeriodAmount]]
