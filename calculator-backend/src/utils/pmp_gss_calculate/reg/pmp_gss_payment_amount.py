a7677368ce44fb9f307165e14c27607849eb416e
Vladislav <winr2005@gmail.com>
2026-03-08 20:26:01 +0500

    Реализация функции pmp_gss_payment_amount без тестирования

--- calculator-backend/src/utils/pmp_gss_calculate/reg/pmp_gss_payment_amount.py
+++ calculator-backend/src/utils/pmp_gss_calculate/reg/pmp_gss_payment_amount.py
@@ -34,9 +34,10 @@ async def pmp_gss_payment_amount(
     suspension_dks = [p.DK for p in suspension_periods]
 
     SP_STANDART: PaymentsByPeriods = await calculate_sp_standart(data)
-
+    
     # SP_STANDART =
     #     0: {
+    #       is_payment_transferred: bool
     # 	    type: PensionCategoryRaw;
     # 	    periods: [
     #                   {DN, DK, amount};
@@ -44,6 +45,7 @@ async def pmp_gss_payment_amount(
     #                 ]
     #     };
     #     1: {
+    #       is_payment_transferred: bool
     # 	    type: PensionCategoryRaw;
     # 	    periods: [
     #                   {DN, DK, amount};
@@ -62,7 +64,12 @@ async def pmp_gss_payment_amount(
             or SP_STANDART[l].type == "departmental"
             or SP_STANDART[l].type == "insurance_SPK"
         ):
-            result = await social_SPK_insurance_SPK_departmental()
+            result = await social_SPK_insurance_SPK_departmental(
+                sp_standart_item=SP_STANDART[l],
+                gss_periods=gss_periods,
+                pmp_periods=pmp_periods,
+                suspension_dks=suspension_dks,
+            )
         else:
             print("Некорректно выбран тип пенсии")
             pass
@@ -78,6 +85,7 @@ async def social_disability(
 
     # SP_STANDART = {
     # 0: {
+    #     is_payment_transferred: bool
     #     type: PensionCategoryRaw;
     #     periods: [
     #               {DN, DK, amount};
@@ -85,6 +93,7 @@ async def social_disability(
     #             ]
     # };
     #     1: {
+    #     is_payment_transferred: bool
     # 	    type: PensionCategoryRaw;
     # 	    periods: [
     #                   {DN, DK, amount};
@@ -121,96 +130,193 @@ async def social_disability(
 
             year = current_date.year
             if current_date.month == 12:
-                amount = gss_standart[year] - sp_year
+                amount = gss_standart[year+1] - sp_year
             else:
                 amount = gss_standart[year + 1] - sp_year
-        if j == 0 or amount > gss_periods_with_amount[i][j - 1].amount:
-            gss_periods_with_amount[i].append(
-                PeriodAmount(DN=current_date, DK=gss_periods[i][j + 1], amount=amount)
-            )
-        else:
-            gss_periods_with_amount[i].append(
-                PeriodAmount(
-                    DN=current_date,
-                    DK=gss_periods[i][j + 1],
-                    amount=gss_periods_with_amount[i][j - 1].amount,
+            if j == 0 or amount > gss_periods_with_amount[i][j - 1].amount:
+                gss_periods_with_amount[i].append(
+                    PeriodAmount(DN=current_date, DK=gss_periods[i][j + 1], amount=amount)
                 )
-            )
+            else:
+                gss_periods_with_amount[i].append(
+                    PeriodAmount(
+                        DN=current_date,
+                        DK=gss_periods[i][j + 1],
+                        amount=gss_periods_with_amount[i][j - 1].amount,
+                    )
+                )
+
+    return gss_periods_with_amount
 
 
-async def social_SPK_insurance_SPK_departmental():
-    pass
 
 
-# async def pmp_gss_payment_amount(
-#     pmp_periods: Dict[str, List[List[date]]],
-#     gss_periods: Dict[str, List[List[date]]],
-#     data: JsonQuerySchema,
-# ) -> dict:
-#     """
-#     Функция преобразования индексированных периодов ПМП и ГСС с расчетом amount
-#     """
-#     suspension_periods = data.periods_suspension
-#     # Извлекаем все даты DK из suspension_periods для быстрого поиска
-#     suspension_dks = [p.DK for p in suspension_periods]
+async def recalculation_gss_amount(
+    sp_standart_item,
+    gss_periods,
+    gss_standart
+    ):
+    
+    # SP_STANDART = {
+    # 0: {
+    #     is_payment_transferred: bool
+    #     type: PensionCategoryRaw;
+    #     periods: [
+    #               {DN, DK, amount};
+    #               {DN, DK, amount}
+    #             ]
+    # };
+    #     1: {
+    #     is_payment_transferred: bool
+    # 	    type: PensionCategoryRaw;
+    # 	    periods: [
+    #                   {DN, DK, amount};
+    #                   {DN, DK, amount}
+    #                 ]
+    #     }
+    # }
 
-#     # Обрабатываем оба типа периодов
-#     processed_pmp = await process_payment_periods(
-#         data,
-#         pmp_periods,
-#         suspension_dks,
-#         "pmp"
-#     )
+    # словарь вида {0: [{"DN": date, "DK": date, "amount": float}, ...]}
+    gss_periods_with_amount = Dict[int, List[PeriodAmount]]
 
-#     processed_gss = await process_payment_periods(
-#         data,
-#         gss_periods,
-#         suspension_dks,
-#         "gss"
-#     )
+    for i in len(gss_periods):
+        gss_periods_with_amount[i] = []
+        for j in len(i):
+            current_date = gss_periods[i][j]
+            for d in len(sp_standart_item.periods):
+                m = len(sp_standart_item.periods)
+                if d == m - 1:
+                    sp_year = 0
+                elif (
+                    sp_standart_item.periods[d].DN
+                    <= current_date
+                    < sp_standart_item.periods[d].DK
+                ):
+                    sp_year = sp_standart_item.periods[d].amount
+                    sp_year_plus_one = sp_standart_item.periods[d+1].amount
 
-#     return {
-#         "pmp_periods": processed_pmp,
-#         "gss_periods": processed_gss
-#     }
-
-
-# async def split_periods_into_pairs_payment_amount(
-#     periods_data: GssPmpIndexType,
-# ) -> Dict[str, Dict[int, dict]]:
-#     """
-#     Преобразует периоды из формата [[d1, d2, d3], [d4, d5]] в формат
-#     {
-#         "0": {
-#             0: {"DN": d1, "DK": d2, "amount": None},
-#             1: {"DN": d2, "DK": d3, "amount": None},
-#             2: {"DN": d4, "DK": d5, "amount": None}
-#         }
-#     }
-#     """
-#     result = {}
-#     for pension_id, period_lists in periods_data.items():
-#         print(pension_id, period_lists)
-
-#         pension_periods = []
-
-#         for date_list in period_lists:
-#             for l in range(len(date_list) - 1):
-#                 pension_periods.append(
-#                     {"DN": date_list[l], "DK": date_list[l + 1], "amount": None}
-#                 )
-
-#         result[pension_id] = pension_periods
-
-#     return result
+            year = current_date.year
+            if current_date.month == 12:
+                amount = gss_standart[year+1] - sp_year_plus_one
+            else:
+                amount = gss_standart[year] - sp_year
+            if j == 0 or amount > gss_periods_with_amount[i][j - 1].amount:
+                gss_periods_with_amount[i].append(
+                    PeriodAmount(DN=current_date, DK=gss_periods[i][j + 1], amount=amount)
+                )
+            else:
+                gss_periods_with_amount[i].append(
+                    PeriodAmount(
+                        DN=current_date,
+                        DK=gss_periods[i][j + 1],
+                        amount=gss_periods_with_amount[i][j - 1].amount,
+                    )
+                )
+    return gss_periods_with_amount
 
 
-# async def pmp_gss_payment_amount(
-#     pmp_periods: GssPmpIndexType, gss_periods: GssPmpIndexType
-# ) -> dict:
-#     """Функция преобразования индексированных периодов ПМП и ГСС в разрезанный формат"""
+async def recalculation_pmp_amount(
+    sp_standart_item,
+    pmp_periods,
+    pmp_standart,
+    suspension_dks
+    ):
+    # SP_STANDART = {
+    # 0: {
+    #     is_payment_transferred: bool
+    #     type: PensionCategoryRaw;
+    #     periods: [
+    #               {DN, DK, amount};
+    #               {DN, DK, amount}
+    #             ]
+    # };
+    #     1: {
+    #     is_payment_transferred: bool
+    # 	    type: PensionCategoryRaw;
+    # 	    periods: [
+    #                   {DN, DK, amount};
+    #                   {DN, DK, amount}
+    #                 ]
+    #     }
+    # }
 
-#     split_pmp = split_periods_into_pairs_payment_amount(pmp_periods)
-#     split_gss = split_periods_into_pairs_payment_amount(gss_periods)
+    # словарь вида {0: [{"DN": date, "DK": date, "amount": float}, ...]}
+    pmp_periods_with_amount = Dict[int, List[PeriodAmount]]
 
-#     return {"pmp_periods": split_pmp, "gss_periods": split_gss}
+    for i in len(pmp_periods):
+        pmp_periods_with_amount[i] = []
+        for j in len(i):
+            current_date = pmp_periods[i][j]
+            for d in len(sp_standart_item.periods):
+                if (
+                    i == 0
+                    and j == 0
+                    and sp_standart_item.is_payment_transferred
+                    and current_date >= sp_standart_item.periods[0].DK
+                ):
+                    sp_year = sp_year_minus_one = sp_standart_item.periods[0].amount
+                else:
+                    m = len(sp_standart_item.periods)
+                    if d == m - 1:
+                        sp_year = 0
+                    elif (
+                        sp_standart_item.periods[d].DN
+                        <= current_date
+                        < sp_standart_item.periods[d].DK
+                    ):
+                        sp_year = sp_standart_item.periods[d].amount
+                        sp_year_minus_one = sp_standart_item.periods[d-1].amount
+
+            year = current_date.year
+            if current_date.month == 12:
+                amount = pmp_standart[year+1] - sp_year
+            else:
+                # Логика проверки на даты возобновлений
+                for k in range(len(suspension_dks)):
+                    if current_date == suspension_dks[k]:
+                        amount = pmp_standart[year] - sp_year_minus_one
+                        break
+                    if k == len(suspension_dks) - 1: 
+                        amount = pmp_standart[year] - sp_year
+                amount = pmp_standart[year + 1] - sp_year
+            if j == 0 or amount > pmp_periods_with_amount[i][j - 1].amount:
+                pmp_periods_with_amount[i].append(
+                    PeriodAmount(DN=current_date, DK=pmp_periods[i][j + 1], amount=amount)
+                )
+            else:
+                pmp_periods_with_amount[i].append(
+                    PeriodAmount(
+                        DN=current_date,
+                        DK=pmp_periods[i][j + 1],
+                        amount=pmp_periods_with_amount[i][j - 1].amount,
+                    )
+                )
+    return pmp_periods_with_amount
+
+async def social_SPK_insurance_SPK_departmental(
+    sp_standart_item: PaymentsByPeriodsItem,
+    gss_periods: GssPmpIndexType,
+    pmp_periods: GssPmpIndexType,
+    suspension_dks
+    ):
+
+    gss_standart = GSS_STANDART
+    pmp_standart = PMP_STANDART
+
+    pmp_periods_amount = recalculation_pmp_amount(
+        sp_standart_item=sp_standart_item,
+        pmp_periods=pmp_periods,
+        pmp_standart=pmp_standart,
+        suspension_dks=suspension_dks,
+        )
+    gss_periods_amount = recalculation_gss_amount(
+        sp_standart_item,
+        gss_periods,
+        gss_standart,
+        ) 
+
+    return {
+        'pmp_periods': pmp_periods_amount,
+        'gss_periods': gss_periods_amount
+    }
+
