567c6a3202a765b866c84433069a56507546754a
Kate <belova.ek0@gmail.com>
2026-03-10 23:18:01 +0500

    Добавление информации о переводах пенсии в calculate_sp_standart

--- calculator-backend/src/utils/payments/types/paymentType.py
+++ calculator-backend/src/utils/payments/types/paymentType.py
@@ -11,6 +11,10 @@ class PeriodAmount(PeriodType):
 
 class PaymentsByPeriodsItem(BaseModel):
     is_payment_transferred: bool
+    is_get_PSD_FSD_last_mounth_payment_trasferred: bool
+    is_get_PSD_FSD_last_year_payment_trasferred: bool
+    is_Not_get_PSD_FSD_now_payment_trasferred: bool
+
     type: PensionCategoryRaw  # "insurance_SPK", "social_SPK", "social_disability", "departmental"
     periods: List[PeriodAmount]  # [DN, DK, amount]
 
